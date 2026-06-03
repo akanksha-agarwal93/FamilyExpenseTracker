1b42583b61a3d1b6421ac898c310322e53557736
Akanksha Agarwal <akanksha.agar@gmail.com>
2026-06-03 14:06:36 +0200

    Formatted files and fixed the typo in Context forlder

--- src/App.tsx
+++ src/App.tsx
@@ -1,40 +1,38 @@
-import './App.css'
-import { AddExpense } from './components/AddExpense';
-import Home from './components/Home';
-import {createBrowserRouter, RouterProvider} from 'react-router-dom';
-import { ExpenseProvider } from './cotext/ExpenseContext';
-import Expenses from './components/Expenses';
-import { EditExpense } from './components/EditExpense';
-
+import "./App.css"
+import { AddExpense } from "./components/AddExpense"
+import Home from "./components/Home"
+import { createBrowserRouter, RouterProvider } from "react-router-dom"
+import { ExpenseProvider } from "./context/ExpenseContext"
+import Expenses from "./components/Expenses"
+import { EditExpense } from "./components/EditExpense"
 
 function App() {
-const router = createBrowserRouter([
-  {
-    path: '/',
-    element: <Home/>,
-    errorElement: <div>Page Not Found</div>
-  },
-  {
-   path: '/add-expense',
-   element: <AddExpense/> 
-  },
-  {
-    path: '/expenses',
-    element: <Expenses/>
-  },
-  {
-    path: '/edit-expense/:id',
-    element: <EditExpense />
-  },
-  
-])
-  return (
-    <>
-      <ExpenseProvider>
-        <RouterProvider router={router}/>
-      </ExpenseProvider>
-    </>
-  )
+	const router = createBrowserRouter([
+		{
+			path: "/",
+			element: <Home />,
+			errorElement: <div>Page Not Found</div>,
+		},
+		{
+			path: "/add-expense",
+			element: <AddExpense />,
+		},
+		{
+			path: "/expenses",
+			element: <Expenses />,
+		},
+		{
+			path: "/edit-expense/:id",
+			element: <EditExpense />,
+		},
+	])
+	return (
+		<>
+			<ExpenseProvider>
+				<RouterProvider router={router} />
+			</ExpenseProvider>
+		</>
+	)
 }
 
 export default App
--- src/components/Expenses.tsx
+++ src/components/Expenses.tsx
@@ -1,5 +1,5 @@
 import { useNavigate } from "react-router-dom"
-import { useExpenses } from "../cotext/ExpenseContext"
+import { useExpenses } from "../context/ExpenseContext"
 import { CiEdit } from "react-icons/ci"
 import { MdOutlineDelete } from "react-icons/md"
 
